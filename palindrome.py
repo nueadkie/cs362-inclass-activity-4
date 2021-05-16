cb6ed55e064a2371d580461aa593b5031a62d1aa
Chris Miraflor <chris.j.miraflor@gmail.com>
2021-05-15 20:35:55 -0700

    palindrome checker now ignores case

--- palindrome.py
+++ palindrome.py
@@ -1,5 +1,5 @@
 def is_palindrome(input_string):
   for i in range(len(input_string) // 2):
-    if(input_string[i] != input_string[len(input_string) - i - 1]):
+    if(input_string[i].lower() != input_string[len(input_string) - i - 1].lower()):
       return False
   return True
